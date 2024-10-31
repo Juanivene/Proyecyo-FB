252f983933d06114bdc7f3d84373b95f402d6043
Juanivene <168385555+Juanivene@users.noreply.github.com>
2024-10-31 12:15:35 -0300

    agrego card cliente estructurada con datos dinamicos y botones de continuar y regresar a datos personales

--- src/Views/ConfirmationView.jsx
+++ src/Views/ConfirmationView.jsx
@@ -1,8 +1,9 @@
 import Grid from "../Components/Grid/grid";
-import { useMediaQuery } from "react-responsive";
 import PayCardInfo from "../Components/Pay/PayCardInfo";
 import ConfirmCardDataClient from "../Components/Confirmation/ConfirmCardDataClient";
 import ConfirmCardFlight from "../Components/Confirmation/ConfirmCardFlight";
+import { useMediaQuery } from "react-responsive";
+import { Link } from "react-router-dom";
 
 const ConfirmationView = () => {
   const urlParams = new URLSearchParams(window.location.search);
@@ -13,7 +14,7 @@ const ConfirmationView = () => {
     return customer.id === idCustomerSelected;
   });
 
-  const isMediumScreen = useMediaQuery({ maxWidth: 850, maxHeight: 641 });
+  const responsive = useMediaQuery({ maxWidth: 850, maxHeight: 641 });
 
   return (
     <Grid container gap={3} className={`m-10`}>
@@ -24,18 +25,24 @@ const ConfirmationView = () => {
         </div>
       </Grid>
 
-      <Grid item xs={12} sm={12} md={12} lg={7} xl={7}>
+      <Grid item xs={12} sm={12} md={12} lg={7} xl={7} className={`space-y-3`}>
         <ConfirmCardFlight customerSelcted={customerSelected} />
+        <ConfirmCardDataClient customerSelcted={customerSelected} />
       </Grid>
-
-      {isMediumScreen ? null : (
+      {!responsive ? (
         <Grid item xs={12} sm={12} md={12} lg={5} className={`sticky top-20`}>
           <PayCardInfo id={customerSelected.flightSelected.id} />
         </Grid>
+      ) : (
+        ""
       )}
-
-      <Grid item xs={12} sm={12} md={12} lg={8}>
-        <ConfirmCardDataClient />
+      <Grid item xs={7} className={`flex justify-between`}>
+        <Link to="/pay" className="link link-warning hidden md:block">
+          Datos personales
+        </Link>
+        <button  className="btn btn-active btn-wide ">
+          Continuar
+        </button>
       </Grid>
     </Grid>
   );
